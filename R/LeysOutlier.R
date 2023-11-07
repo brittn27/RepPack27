29ce2bda89caeaceb3b6ec7dc0ca77de0b7704a4
brittn27 <brittn@mcmaster.ca>
2023-11-06 21:39:54 -0500

    seeing if printing works

--- R/LeysOutlier.R
+++ R/LeysOutlier.R
@@ -35,6 +35,7 @@ LeysOutlier <- function(x, y, z){
   NewData <- subset(OldData, OldData$z> low_cutoff5 & OldData$z < high_cutoff5)
   write.csv(NewData,"PostOutlierRT.csv",sep=" ")
   Good_Data <- read.csv("PostOutlierRT.csv")
+  print(MAD5)
 }
 
 
