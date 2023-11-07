cda80684c1baf2a0727528682efd06c6796c34e3
brittn27 <brittn@mcmaster.ca>
2023-11-06 20:58:35 -0500

    Added factors for the data.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,6 +3,9 @@
 library(readr)
 RTDATA <- read_csv("data-raw/Data1.csv")
 RTDATA <- RTDATA[c(1:6,17:19)]
+RTDATA$depth <- as.factor(RTDATA$depth)
+RTDATA$difficulty <- as.factor(RTDATA$difficulty)
+RTDATA$RT <- RTDATA$key_resp.rt
 
 
 usethis::use_data(RTDATA, overwrite = TRUE)
--- tests/testthat/test-LeysOutlier.R
+++ tests/testthat/test-LeysOutlier.R
@@ -1,3 +1,3 @@
-test_that("outlier method works", {
+test_that("outlier works", {
   expect_equal(2 * 2, 4)
 })
